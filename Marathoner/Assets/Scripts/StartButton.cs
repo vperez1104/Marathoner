4060b0c048542a382e829226379726ca61208075
ptran1125 <phong.t1125@gmail.com>
2015-03-22 01:56:52 -0500

    messed up something lol
    messed up

---
+++ Marathoner/Assets/Scripts/StartButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+
+	// Update is called once per frame
+	public void StartGame (string sceneschange) {
+		Application.LoadLevel (sceneschange);
+	
+	}
+}
